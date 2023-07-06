f8fb213c67231ecf190e0f086d75a99910081f74
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-06 16:13:23 +0300

    pointers

---
+++ pointers/pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+using std::string;
+
+
+
+
+int main () {
+    int x=5, y=9, sum;
+
+    sum = x + y;
+    cout << "The sum is: " << sum << endl;
+    return 0;
+
+}
