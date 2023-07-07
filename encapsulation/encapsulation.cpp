e28bfebd3a9a0074870cd0803796bf3addda76c7
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-07 04:22:24 +0300

    encapsulation

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -8,12 +8,42 @@ using std::string;
 // this way the implementation details of the class can be hidden from the user
 class Employee
 {
-public:
+private:
     string Name;
     string Company;
     int Age;
 
 public:
+    void setName(string name){
+        this->Name = name;
+    };
+    //getter
+    string getName(){
+        return Name;
+    };
+
+    void setCompany( string company){
+        this->Company = company;
+    };
+
+    string getCompany(){
+        return Company;
+    };
+
+    void setAge( int age){ 
+        // age > 18 ? this->Age = age : throw("validation error");
+        if(age!= 18){
+            cout << "age has to be greater than 18";
+        } else{
+            this->Age = age;
+        }
+    };
+
+    int getAge(){
+        return Age;
+    }
+
+
     void introduce_yourself()
     {
         cout << "Name\t" << Name << endl;
@@ -30,6 +60,14 @@ public:
 
 int main()
 {
-    Employee employee1 = Employee("Martin", "marvel",  24);
-    employee1.introduce_yourself();
+    Employee employee1 = Employee("Martin", "marvel",  0);
+    Employee employee2 = Employee("console", "javascript",  0);
+    // employee1.introduce_yourself();
+    
+
+    //tests the setters
+    employee1.setAge(4);
+    cout << employee1.getName()<<"\t"<< "is\t"<< employee1.getAge() << endl;
+    employee2.setAge(10);
+    cout << employee2.getName()<<"\t"<< "is\t"<< employee2.getAge() << endl;
 }
